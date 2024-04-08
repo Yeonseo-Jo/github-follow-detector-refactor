486e231460c154cc745c43b077596c8fa0b737de
Yeonseo-Jo <jys073190@gmail.com>
2024-04-09 03:00:39 +0900

    feat: 홈 화면 ui 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
+import * as styles from "../../styles/home/HomeContainerStyle.css.ts";
+
 const HomePage = () => {
-  return <div>home(login 할 부분)</div>;
+  return (
+    <section className={styles.HomeWrapper}>
+      <article className={styles.TokenBoxContainer}>
+        <div className={styles.TokenLinkBox}>
+          <Link
+            href={"https://github.com/settings/tokens"}
+            className={styles.TokenLinkBtn}
+          >
+            Github Token 만들러 가기
+          </Link>
+          <p className={styles.TokenInfoText}>
+            ﹒ 토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!
+          </p>
+        </div>
+        <div className={styles.TokenInputBox}>
+          <input
+            type="text"
+            placeholder="Github Token을 입력해주세요"
+            className={styles.TokenInput}
+          />
+          <button type="button" className={styles.TokenInputNextBtn}>
+            나의 맞팔 확인하기
+          </button>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default HomePage;
