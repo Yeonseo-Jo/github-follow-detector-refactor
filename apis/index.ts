6c317131d3f1c0141b6ac1cade55f7f7eab70d67
Yeonseo-Jo <jys073190@gmail.com>
2024-04-09 17:59:16 +0900

    fix : axios instance 수정 및 instance defaults를 통해 토큰 저장 함수 생성

--- apis/index.ts
+++ apis/index.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosInstance } from "axios";
 
-export const client = (token: string) => {
-  const client: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${token}`,
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
+export const instance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    Accept: "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+});
 
-  return client;
+// token 입력 후 헤더에 저장하는 함수
+export const setInstanceToken = (token: string) => {
+  instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
