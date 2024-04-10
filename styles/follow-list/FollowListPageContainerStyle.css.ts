5acad80e7371150f8d4f98c38dc9edf90b90ffc1
Yeonseo-Jo <jys073190@gmail.com>
2024-04-10 22:49:14 +0900

    feat: followListPage css 추가

---
+++ styles/follow-list/FollowListPageContainerStyle.css.ts
@@ -0,0 +1,17 @@
+import { style } from "@vanilla-extract/css";
+
+export const FollowListPageWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+
+  width: "100%",
+  height: "100%",
+  margin: "2rem 0",
+
+  fontWeight: "bold",
+  fontSize: "3.5rem",
+
+  gap: "2rem",
+});
