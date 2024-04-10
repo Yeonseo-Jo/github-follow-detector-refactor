b36c384fe51148945ab389f091dcd27245a0c902
Yeonseo-Jo <jys073190@gmail.com>
2024-04-10 21:05:45 +0900

    feat : user 프로필 부분 데이터 연결

--- app/follow-list/page.tsx
+++ app/follow-list/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import UserProfile from "@/components/follow-list/UserProfile";
 import { useGetCombinedUserInfo } from "@/hooks/user/useGetCombinedUserInfo";
 
 const FollowListPage = () => {
   const { isLoading, error, data } = useGetCombinedUserInfo();
 
+  const userProfileData = data[0];
+
   console.log(isLoading, error, data);
-  return <div>follow-list</div>;
+
+  return (
+    <>
+      {isLoading && <div>로딩중..</div>}
+      {error && <div>에러 발생!</div>}
+      {!isLoading && !error && <UserProfile userProfile={userProfileData} />}
+    </>
+  );
 };
 
 export default FollowListPage;
