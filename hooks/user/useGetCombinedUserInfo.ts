87f6979e00eb9b54c09f7c2ff5a0cd2adc205b73
Yeonseo-Jo <jys073190@gmail.com>
2024-04-10 21:05:23 +0900

    fix: useGetCombinedUserInfo data 타입 세부 지정

--- hooks/user/useGetCombinedUserInfo.ts
+++ hooks/user/useGetCombinedUserInfo.ts
@@ -1,5 +1,6 @@
 import { getUserFollowInfo } from "@/apis/user/getUserFollowInfo";
 import { getUserProfileInfo } from "@/apis/user/getUserProfileInfo";
+import { UserProfileInfoTypes, UserTypes } from "@/types/user/UserTypes";
 import { useQueries } from "@tanstack/react-query";
 
 export const useGetCombinedUserInfo = () => {
@@ -12,7 +13,13 @@ export const useGetCombinedUserInfo = () => {
       return {
         isLoading: results.some((result) => result.isLoading),
         error: results.some((result) => result.isError),
-        data: results.map((result) => result.data),
+        data: results.map((result) => result.data) as [
+          UserProfileInfoTypes,
+          {
+            followingData: UserTypes[];
+            followersData: UserTypes[];
+          }
+        ],
       };
     },
   });
