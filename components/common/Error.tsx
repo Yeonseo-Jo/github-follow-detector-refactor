5916a3552e7d4c0fccc035b9aaae0a55a27f84bb
Yeonseo-Jo <jys073190@gmail.com>
2024-04-17 16:30:53 +0900

    fix: 에러시 토큰 삭제 로직 추가

--- components/common/Error.tsx
+++ components/common/Error.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import * as styles from "@/styles/common/ErrorStyle.css";
+import { getSessionStorageHandler } from "@/utils/getSessionStorageHandler";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Error = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    // 에러 시 sessionStorage에 있는 token 정보 삭제
+    if (typeof window === undefined) return;
+    if (!getSessionStorageHandler().hasItem) return;
+
+    getSessionStorageHandler().removeItem("token");
+  }, []);
 
   return (
     <section className={styles.ErrorWrapper}>
